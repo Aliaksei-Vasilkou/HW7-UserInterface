93792ee5f02ddd3595c5d92cc12d5e8632ac35e6
Vasilkov Alexey <coyc@inbox.ru>
2017-11-05 00:43:57 +0300

    Added some image resources. Created top_bar & bottom_bar

--- app/src/main/java/com/github/coyclab/hw7_userinterface/MainActivity.java
+++ app/src/main/java/com/github/coyclab/hw7_userinterface/MainActivity.java
@@ -1,7 +1,11 @@
 package com.github.coyclab.hw7_userinterface;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
